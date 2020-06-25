21dc85902e8efde213ebd757be7f640d74c4fa4c
ahaoboy <504595380@qq.com>
2020-06-25 23:04:17 +0800

    add parcel

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ b站挂机刷经验脚本
 自动投币
 
 需要Cookie
+
+
+yarn parcel index.html
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {enc, dec} from "./util";
+
+
+console.log('hello')
+
+console.log(enc(498566183))
+console.log(dec('BV1AK411W7wq'))
