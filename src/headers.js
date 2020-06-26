0f2796a944864a2c64ac0c4af053dfbfd088ce48
ahaoboy <504595380@qq.com>
2020-06-26 13:12:46 +0800

    fix csrf

--- src/headers.js
+++ src/headers.js
@@ -1,6 +1,7 @@
 import {env} from "process";
 
 const BILIBILI_COOKIE = env.BILIBILI_COOKIE
+console.log('BILIBILI_COOKIE', BILIBILI_COOKIE)
 const headers = {
   "accept": "*/*",
   "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
@@ -16,8 +17,9 @@ const s = headers['cookie']
 
 let re = /bili_jct=(.*?);/;
 let list = re.exec(s);
+console.log('re', re.exec(s))
 const csrf = list[1];
-
+console.log('csrf', csrf)
 export {
   headers,
   csrf
