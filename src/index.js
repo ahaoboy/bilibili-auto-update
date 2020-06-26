59f63f7facf4ab00241ab53a3aedbe08254f4242
ahaoboy <504595380@qq.com>
2020-06-26 11:59:23 +0800

    add cookie

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import {enc, dec} from "./util";
+import {env} from "process";
 
+console.log('env', env)
+console.log('BILIBILI_COOKIE', env.BILIBILI_COOKIE)
 
 console.log('hello')
-
 console.log(enc(498566183))
 console.log(dec('BV1AK411W7wq'))
